d34ee1e0c3aeac4a23b388b9956c448bf440f82b
FernandoSa93 <fernando.de.sa@hotmail.com>
2019-03-21 01:07:51 -0300

    Commitado esboço do menu que fizemos em aula, aguardando implementação do Fabio.

---
+++ applications_menu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  int opcao = 0;
+  while (opcao != 5) {
+    printf("<<<< Aplications Menu >>>\n"
+           "      1) Web Browser\n"
+           "      2) Text Editor\n"
+           "      3) Terminal\n"
+           "      4) Finalizar Processo\n"
+           "      5) Quit\n"
+           "Opção: ");
+    scanf("%d", &opcao);
+  }
+}
