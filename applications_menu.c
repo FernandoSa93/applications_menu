fd195f28293e55ec2abefcd4fd9caf15c7892b8e
FernandoSa93 <fernando.de.sa@hotmail.com>
2019-03-26 15:23:47 -0300

    Incluído trecho da chamada do text editor, sinalizado duas partes do código para o autor verificar e ajustado o printf do menu para se comportar corretamente com as variáveis de status.

--- applications_menu.c
+++ applications_menu.c
@@ -14,6 +14,10 @@
 /*********************************************************************************************************************************************/
 //global variables
 int input_value=0;
+char statusWebBrowser[50]="Parado",
+       statusTextEditor[50]="Parado",
+       statusTerminal[50]="Parado",
+       statusFinalizarProcesso[50]=" ";
 
 /*********************************************************************************************************************************************/
 //home menu (display default)
@@ -21,23 +25,33 @@ void home (void)
 {
   system("clear\n\n");
   //      12345678901234567890123456789012345678901234567890123456789012
-  printf("    **************************************************************\n");
-  printf("    **                <<<< Aplications Menu >>>>                **\n");
-  printf("    **************************************************************\n");
-  printf("    **                              |                           **\n");
-  printf("    **     1) Web Browser           |                           **\n");
-  printf("    **     2) Text Editor           |                           **\n");
-  printf("    **     3) Terminal              |                           **\n");
-  printf("    **     4) Finalizar Processo    |                           **\n");
-  printf("    **     5) Quit                  |                           **\n");
-  printf("    **                              |                           **\n");
-  printf("    **************************************************************\n");
-  printf("    **            Copyright Fábio, Fernando e Nadine            **\n");
-  printf("    **************************************************************\n");
+  printf("    **************************************************************\n"
+         "    **                <<<< Aplications Menu >>>>                **\n"
+         "    **************************************************************\n"
+         "    **                              |                           **\n"
+         "    **     1) Web Browser           | %-26s**\n"
+         "    **     2) Text Editor           | %-26s**\n"
+         "    **     3) Terminal              | %-26s**\n"
+         "    **     4) Finalizar Processo    | %-26s**\n"
+         "    **     5) Quit                  |                           **\n"
+         "    **                              |                           **\n"
+         "    **************************************************************\n"
+         "    **            Copyright Fábio, Fernando e Nadine            **\n"
+         "    **************************************************************\n"
+         , statusWebBrowser, statusTextEditor, statusTerminal, statusFinalizarProcesso);
 }
 
 /*********************************************************************************************************************************************/
 //menu refresh (update display)
+
+/*********************************************************************************************************************************************/
+//executa Text Editor
+void executaTextEditor()
+{
+  system("gedit&");
+  sleep(1);
+  strcpy(statusTextEditor, "Executando, PID=");
+}
 
 /*********************************************************************************************************************************************/
 //input reading
@@ -52,12 +66,16 @@ int input_reading(void)
     case 1:
         printf("=1");
         break;
+    case 2:
+        executaTextEditor();
+        break;
     case 5:
-        printf("Opção 'Quit' selecionado!");
+        printf("Opção 'Quit' selecionada!");
         break;
     default:
-        printf("Dado inválido!");
-        input_value=5; 
+        printf("Dado inválido! Selecione uma opção entre 1 e 5.\n");
+//   ATENÇÃO!!!!
+//        input_value=5;    (Acredito que aqui não seja necessário parar a execução...)
         break;
   }
   printf("\n");
@@ -70,8 +88,10 @@ int input_reading(void)
 //main code
 int main(void)//void or int?
 {
-   home();
-   input_value=input_reading();
+//    ATENÇÃO!!!!
+//   home();                        (home() e input_reading() estavam sendo chamados aqui e dentro do loop,
+//   input_value=input_reading();   duplicando saídas como o quit, sendo necessário digitar 5 duas vezes para encerrar a execução...)
+
 //Start home menu and input reading. It does while there isn't the value in the input.
   do
   {
@@ -80,7 +100,7 @@ int main(void)//void or int?
   } while (input_value!=5);
 
   printf("Até a próxima!\n\n");
-  sleep(1);
+  sleep(2);
 //if int main
   return 0;
 }
