b1aabee4601211436fa1ae5415a22e303bb63b9b
Fabexseven <48931263+Fabexseven@users.noreply.github.com>
2019-03-26 13:21:09 -0300

    Update applications_menu.c

--- applications_menu.c
+++ applications_menu.c
@@ -1,49 +1,86 @@
+/*********************************************************************************************************************************************
+****Laborátorio de Sistemas Operacionais
+****      Trabalho do Grau A (TGA)
+****  Autores: Fábio, Fernando, Nadine
+****      Turma: GR16031-00011 
+*********************************************************************************************************************************************/
+//includes
 #include <stdio.h>
 #include <stdlib.h>
 //#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
 
-//Esboço do menu
-int main(){
-  int opcao = 0;
-  char statusWebBrowser[50]="Parado",
-       statusTextEditor[50]="Parado",
-       statusTerminal[50]="Parado", 
-       statusFinalizarProcesso[50]=" ";
-  
-  //Funções
-  void executaTextEditor(){
-    system("gedit&");
-    sleep(1);
-    strcpy(statusTextEditor, "Executando, PID=");
-  }
+/*********************************************************************************************************************************************/
+//global variables
+int input_value=0;
 
-  void quit(){
-    sleep(1);
-    printf("Bye\n\n");
-  }
-  
-  while (opcao != 5){
-    system("clear");
-    printf("<<<< Applications Menu >>>\n"
-           "      1) Web Browser         (%s)\n"
-           "      2) Text Editor         (%s)\n"
-           "      3) Terminal            (%s)\n"
-           "      4) Finalizar Processo  (%s)\n"
-           "      5) Quit\n"
-           "Opção: ", statusWebBrowser, statusTextEditor, statusTerminal, statusFinalizarProcesso);
-    scanf("%d", &opcao);
+/*********************************************************************************************************************************************/
+//home menu (display default)
+void home (void)
+{
+  system("clear\n\n");
+  //      12345678901234567890123456789012345678901234567890123456789012
+  printf("    **************************************************************\n");
+  printf("    **                <<<< Aplications Menu >>>>                **\n");
+  printf("    **************************************************************\n");
+  printf("    **                              |                           **\n");
+  printf("    **     1) Web Browser           |                           **\n");
+  printf("    **     2) Text Editor           |                           **\n");
+  printf("    **     3) Terminal              |                           **\n");
+  printf("    **     4) Finalizar Processo    |                           **\n");
+  printf("    **     5) Quit                  |                           **\n");
+  printf("    **                              |                           **\n");
+  printf("    **************************************************************\n");
+  printf("    **            Copyright Fábio, Fernando e Nadine            **\n");
+  printf("    **************************************************************\n");
+}
 
-    if (opcao>5 || opcao<1){
-      printf("Comando inválido! Selecione uma opção entre 1 e 5.\n");
-      sleep(2);
-      continue;
-    }
+/*********************************************************************************************************************************************/
+//menu refresh (update display)
 
-    switch(opcao){
-      case 2: executaTextEditor();
-      case 5: quit();
-    }
+/*********************************************************************************************************************************************/
+//input reading
+int input_reading(void)
+{
+  input_value=0;
+  printf("\n        Opção: ");
+  scanf("%d",&input_value);
+  printf("\n");
+  switch(input_value)
+  {
+    case 1:
+        printf("=1");
+        break;
+    case 5:
+        printf("Opção 'Quit' selecionado!");
+        break;
+    default:
+        printf("Dado inválido!");
+        input_value=5; 
+        break;
   }
+  printf("\n");
+  sleep(1);
+
+  return input_value;
+}
+
+/*********************************************************************************************************************************************/
+//main code
+int main(void)//void or int?
+{
+   home();
+   input_value=input_reading();
+//Start home menu and input reading. It does while there isn't the value in the input.
+  do
+  {
+    home();
+    input_value=input_reading();
+  } while (input_value!=5);
+
+  printf("Até a próxima!\n\n");
+  sleep(1);
+//if int main
+  return 0;
 }
